94055bcc88680e50f05bf6f04eaecee99126c62f
sourabhsingh0 <99664280+sourabhsingh0@users.noreply.github.com>
2025-01-20 14:51:06 +0530

    Add files via upload

---
+++ src/rectangle.py
@@ -0,0 +1,27 @@
+"""
+Problem Description:
+
+Create a Rectangle class that has the following methods:
+
+1. __init__(self, width, height): Initializes a rectangle with given width and height.
+2. area(self): Returns the area of the rectangle.
+3. perimeter(self): Returns the perimeter of the rectangle.
+4. is_square(self): Returns True if the rectangle is a square (i.e., width == height), otherwise returns False.
+"""
+# rectangle.py
+class Rectangle:
+    def __init__(self, width, height):
+        # Initialize the rectangle with width and height
+        pass
+
+    def area(self):
+        # Return the area of the rectangle
+        pass
+
+    def perimeter(self):
+        # Return the perimeter of the rectangle
+        pass
+
+    def is_square(self):
+        # Return True if the rectangle is a square, otherwise False
+        pass
